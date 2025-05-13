646e48c4672d65d482cd179009decfab3a3a9297
henrique-costa <henrique_vc@hotmail.com>
2025-05-12 21:27:50 -0300

    first-challenge

--- pythonAlgorithm/algo.py
+++ pythonAlgorithm/algo.py
@@ -1,1 +1,16 @@
 # This is a single-line comment
+def pular_nuvens(nuvens):
+    saltos = 0
+    indice = 0
+    while indice < (len(nuvens) - 2):
+        if nuvens[indice + 2] != 1:
+            indice += 2
+            saltos += 1
+        else:
+            indice += 1
+            saltos += 1
+    return saltos
+
+# nuvens = [0, 0, 1, 0, 0, 1, 0]
+nuvens = [0, 0, 0, 0, 1, 0]
+print(pular_nuvens(nuvens))
