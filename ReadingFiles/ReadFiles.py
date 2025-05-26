df47078a8a0c463642dd6410224e4f6d9e739a0f
henrique-costa <henrique_vc@hotmail.com>
2025-05-26 16:26:43 -0300

    new-folder-for-reading-files

---
+++ ReadingFiles/ReadFiles.py
@@ -0,0 +1,43 @@
+import csv
+import pandas as pd
+
+archive = open("Products.txt", 'rt')
+
+for line in archive.readlines():
+     print(line)
+     
+print("\nend txt file read\n")
+
+archive = open("Products.txt", 'rt')
+
+print(archive.readline())
+print(archive.readline())
+print(archive.readline())
+print(archive.readline())
+
+print("\nend txt file read line by line\n")
+
+
+leitorCSV = csv.reader('mycsv.csv')
+
+# with open('mycsv.csv', newline='') as csvfile:
+#     spamreader = csv.reader(csvfile)
+#     for row in spamreader:
+#         print(', '.join(row))
+
+with open('mycsv.csv', newline='', encoding='utf-8-sig') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';')
+    for row in spamreader:
+        print(', '.join(row))
+
+
+print("\nend csv file read\n")
+
+
+# lê todas as planilhas (retorna dict de DataFrames)
+dfs = pd.read_excel("mycsv.xlsx", sheet_name=None)
+
+# ou lê apenas a 1ª planilha como DataFrame
+df = pd.read_excel("mycsv.xlsx", sheet_name=0)
+
+print(df.head())
